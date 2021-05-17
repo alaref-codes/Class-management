fe4a8a554422a50bc696e8c195af163f3fe8cf56
alaref-codes <alarefabushaala@gmail.com>
2021-05-17 21:01:50 +0200

    Added the mvc architecture

--- back_.js
+++ back_.js
@@ -1,95 +1,131 @@
-window.onload = function() {
-    document.getElementById("btn").addEventListener("click", onSubmit)
+// window.onload = function() {
+//     document.getElementById("btn").addEventListener("click", onSuِbmit)
+//     buildTable(list)
+// }
+
+// function onSubmit() { 
+//     var formData = Model();
+//     View(formData);
+//     addtolist(formData)
+//     resetForm()
+// }
+
+// document.addEventListener('click', (e) => {
+//     console.log(e.target);
+//     if(e.target.classList.contains('delete')) {
+//         onDelete(e)
+//     } else {
+//         console.log('not')
+//     }
+// })
+
+
+function Model() { 
     var list = [
-        {
-            'code': 234,
-            'tables': 32,
-            'projector': "متوفر",
-            'devices': 43,
-            'charis': 31,
-            'floor': "الأول"
+    ];  
+    
+    return {
+        addtolist: function (formData) {
+            list.push(formData);
         },
-        {
-            'code': 234,
-            'tables': 32,
-            'projector': "غير متوفر",
-            'devices': 43,
-            'charis': 31,
-            'floor': "الثاني"
+    }
+};
+
+function View() {
+    var DOMstrings = {
+        btn: "btn",
+        table: "table",
+        code: "code",
+        desks:"desks",
+        projector:"projector",
+        devices:"devices",
+        chairs:"chairs",
+        floor:"floor"
+    }
+    // var formData = {};
+    // formData[DOMstrings.code] = document.getElementById(DOMstrings.code).value.trim();
+    // formData[DOMstrings.desks] = document.getElementById(DOMstrings.desks).value.trim();
+    // formData[DOMstrings.projector] = document.getElementById(DOMstrings.projector).value.trim();
+    // formData[DOMstrings.devices] = document.getElementById(DOMstrings.devices).value.trim();
+    // formData[DOMstrings.chairs] = document.getElementById(DOMstrings.chairs).value.trim();
+    // formData[DOMstrings.floor] = document.getElementById(DOMstrings.floor).value.trim();
+
+    return {
+        addtoList: function (data) {
+            var table = document.getElementById("table").getElementsByTagName("tbody")[0];
+            var newRow = table.insertRow(table.length);
+            cell1 = newRow.insertCell(0);
+            cell1.innerHTML = data.code;
+            cell2 = newRow.insertCell(1);
+            cell2.innerHTML = data.desks;  
+            cell3 = newRow.insertCell(2);
+            cell3.innerHTML = data.projector;
+            cell4 = newRow.insertCell(3);
+            cell4.innerHTML = data.devices;
+            cell5 = newRow.insertCell(4);
+            cell5.innerHTML = data.chairs;
+            cell6 = newRow.insertCell(5);
+            cell6.innerHTML = data.floor;
+            cell7 = newRow.insertCell(6);
+            cell7.innerHTML = '<a class="delete">مسح</a>';
+       
+        },
+        getDOMstrings: function () {
+            return DOMstrings;
+        },
+        getInput: function () {
+            return formData;
         }
-    ];
-    
-    
-    buildTable(list)
-    
-}
-function buildTable(data){
-    var table = document.getElementById('table')
-    
-    for (var i = 0; i < data.length; i++){
-        var row = `<tr>
-                        <td>${data[i].code}</td>
-                        <td>${data[i].tables}</td>
-                        <td>${data[i].projector}</td>
-                        <td>${data[i].devices}</td>
-                        <td>${data[i].chairs}</td>
-                        <td>${data[i].floor}</td>
-                    </tr>`
-        table.innerHTML += row
-    
-    
     }
+    
+};
+
+function Controller(data,view) { 
+    function setupEventListeners () {
+        var DOM = view.getDOMstrings();
+        // document.querySelector(DOM.btn).addEventListener('click', AddItem);
+        document.getElementById(DOM.btn).addEventListener('click', AddItem);
     }
 
-function onSubmit() { 
-    var formData = model();
-    view(formData);
-    resetForm()
-}
 
-
-
-document.addEventListener('click', (e) => {
-    console.log(e.target);
-    if(e.target.classList.contains('delete')) {
-        onDelete(e)
-    } else {
-        console.log('not')
+    
+    var AddItem = function () {
+        var DOM = view.getDOMstrings();
+        var input = {};
+        input[DOM.code] = document.getElementById(DOM.code).value.trim();
+        input[DOM.desks] = document.getElementById(DOM.desks).value.trim();
+        input[DOM.projector] = document.getElementById(DOM.projector).value.trim();
+        input[DOM.devices] = document.getElementById(DOM.devices).value.trim();
+        input[DOM.chairs] = document.getElementById(DOM.chairs).value.trim();
+        input[DOM.floor] = document.getElementById(DOM.floor).value.trim();
+        // var input;
+        // input = view.getInput();
+        // if (input.code == ''){
+        //     console.log("something");
+        // } else {
+        //     console.log(input.code);
+        // }  
+        if (input.code !== "" && input.desks !== "" && input.projector !== "" && input.devices !== "" && input.chairs !== "" && input.floor !== "" ) {
+            data.addtolist(input);
+            view.addtoList(input);
+            // data.getLastItem()
+        } else {
+            alert('you must write something');
+        }
     }
-})
 
-function model() { 
-    var formData = {};
-    formData["code"] = document.getElementById("code").value.trim();
-    formData["desks"] = document.getElementById("desks").value.trim();
-    formData["projector"] = document.getElementById("projector").value.trim();
-    formData["devices"] = document.getElementById("devices").value.trim();
-    formData["chairs"] = document.getElementById("chairs").value.trim();
-    formData["floor"] = document.getElementById("floor").value.trim();
-    return formData;
+    return {
+        init: function () {
+            setupEventListeners();
+        }
 }
+};
 
-function view(data) {
-    if (data["code"] !== "" && data["desks"] !== "" && data["projector"] !== "" && data["devices"] !== "" && data["chairs"] !== "" && data["floor"] !== "" ) {
-        var table = document.getElementById("table").getElementsByTagName("tbody")[0];
-        var newRow = table.insertRow(table.length);
-        cell1 = newRow.insertCell(0);
-        cell1.innerHTML = data.code;
-        cell2 = newRow.insertCell(1);
-        cell2.innerHTML = data.desks;  
-        cell3 = newRow.insertCell(2);
-        cell3.innerHTML = data.projector;
-        cell4 = newRow.insertCell(3);
-        cell4.innerHTML = data.devices;
-        cell5 = newRow.insertCell(4);
-        cell5.innerHTML = data.chairs;
-        cell6 = newRow.insertCell(5);
-        cell6.innerHTML = data.floor;
-        cell7 = newRow.insertCell(6);
-        cell7.innerHTML = '<a class="delete">مسح</a>';
-    }else{
-        alert("All the fields are required");
-    }   
+window.onload = function() {
+    data = new Model();
+    view = new View();
+    controller = new Controller(data, view);
+    controller.init(); 
 }
 
 function resetForm() {
