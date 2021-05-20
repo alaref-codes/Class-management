7e7f7f590d965bc46f684c0e3903595ef2fea983
alaref-codes <alarefabushaala@gmail.com>
2021-05-20 12:03:59 +0200

    fixed the initial value of the localStorage problem

--- backend/model.js
+++ backend/model.js
@@ -1,6 +1,26 @@
 export default function Model() { 
-    var list = JSON.parse(localStorage.getItem('class')); 
-    
+    var list = [];
+
+
+    var storageValue = JSON.parse(localStorage.getItem('class'));
+
+    if (storageValue !== null) {
+        list = storageValue;
+    }
+
+        
+    if (list.length == 0) {
+        var initalValue = {
+            code: "321",
+            desks: "21",
+            projector: "available",
+            devices: "21",
+            chairs: "21",
+            floor: "first", 
+        }
+        list.push(initalValue)
+    }
+
     return {
         addtolist: function (formData) {
             list.push(formData);
@@ -16,11 +36,13 @@ export default function Model() {
             return list[list.length - 1];
         },
         getItems: function() {
-            // for (var item = 0; item < list.length; item++){ Delete this
-            //     localStorage.setItem('class' , JSON.stringify(list[item]))
-            // }
+
             localStorage.setItem('class' , JSON.stringify(list))
+        
             return list;
+
         },
     }
 };
+
+
