565e8a558ce5c234ce74cf08248e96e06cd40db9
alaref-codes <alarefabushaala@gmail.com>
2021-05-28 19:58:24 +0200

    shorted the code in the view file

--- backend/view.js
+++ backend/view.js
@@ -13,33 +13,19 @@ export default function View() {
 
     return {
         addtoList: function (data) {
-            
-//            var cells = {};
-//            for (var elem = 1; elem <= 7; elem++ ){
-//                cells[`cl${elem}`] = newRow.insertCell(elem - 1); 
-//            } 
-//
-//            // changeing the innerHTML to each cell in the object.
-//            for(var i = 1; i <= 7; i++){ // we can repalce <=7 with the object length
-//                cells[`cl${elem}`].innerHTML = data.DOMstrings[i+1];
-//            }
             var table = document.getElementById("table").getElementsByTagName("tbody")[0];
             var newRow = table.insertRow(table.length);
-            var cell1 = newRow.insertCell(0);
-            cell1.innerHTML = data.code;
-            var cell2 = newRow.insertCell(1);
-            cell2.innerHTML = data.desks;  
-            var cell3 = newRow.insertCell(2);
-            cell3.innerHTML = data.projector;
-            var cell4 = newRow.insertCell(3);
-            cell4.innerHTML = data.devices;
-            var cell5 = newRow.insertCell(4);
-            cell5.innerHTML = data.chairs;
-            var cell6 = newRow.insertCell(5);
-            cell6.innerHTML = data.floor;
-            var cell7 = newRow.insertCell(6);
-            cell7.innerHTML = '<a class="delete">مسح</a>';
+            console.log()
+           var cells = {};
+           for (var elem = 1; elem <= Object.keys(data).length + 1 ; elem++ ){
+               cells[`cl${elem}`] = newRow.insertCell(elem - 1); 
 
+           } 
+           var cad = ["code","desks","projector","devices","chairs","floor"];
+           for (var i = 1; i <= Object.keys(data).length; i++) {
+               cells[`cl${i}`].innerHTML = data[DOMstrings[cad[i-1]]];
+           }
+            cells[`cl${i}`].innerHTML = '<a class="delete">مسح</a>';  
         },
         getDOMstrings: function () {
             return DOMstrings;
@@ -53,7 +39,7 @@ export default function View() {
         },
         resetForm: function() {
             for (const element in DOMstrings) {
-                if(element === "btn" || element === "table"){continue} //to skip first two items in DOMStrigs
+                if(element === "btn" || element === "table"){continue} 
                 else document.getElementById(element).value = "";
             }
         },
@@ -64,7 +50,7 @@ export default function View() {
         getSelectedData: function(td) {
             var selected = td.target.parentElement.parentElement;
             var  selectedData = {}
-            selectedData[DOMstrings.code] = selected.cells[0].innerHTML;  // Use DOMstrings over here
+            selectedData[DOMstrings.code] = selected.cells[0].innerHTML; 
             selectedData[DOMstrings.desks] =  selected.cells[1].innerHTML;
             selectedData[DOMstrings.projector] =  selected.cells[2].innerHTML;
             selectedData[DOMstrings.devices]=  selected.cells[3].innerHTML;
